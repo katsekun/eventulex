b239234de6d3048b0b23ae3159afa2ab0550b448
Arensibia <39529570+Arensibia@users.noreply.github.com>
2018-05-23 18:52:17 +0200

    Add files via upload

---
+++ application/views/eventCabecera.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="utf-8">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>  
+</head>
+
+<body>
+<div class="container text-center">
+	<h1>EVENTULEX</h1>      
+    <h3>Tus entradas sin esperas</h3>
+</div>
+<div class="navbar-header">
+	<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+		<span class="icon-bar" style="background-color: white;"></span>
+	    <span class="icon-bar" style="background-color: white;"></span>
+	    <span class="icon-bar" style="background-color: white;"></span>            
+	</button>   
+</div>
+    
+<div class="navbar navbar-light"  id="myNavbar">  
+    <ul class="nav navbar-nav">
+    	<li class="active"><a href="<?php echo site_url();?>/    /eventos/">EVENTOS</a></li>
+        <li><a href="<?php echo site_url();?>/   /quienesSomos/">QUIENES SOMOS</a></li>
+        <li><a href="<?php echo site_url();?>/    /comoFunciona/">COMO FUNCIONAMOS</a></li>
+    </ul>
+    <ul class="nav navbar-nav navbar-right">
+      <li><a href="<?php echo site_url();?>/  /acceso">USUARIO</a></li>
+      <li><a href="<?php echo site_url();?>/  /nuevo">NUEVO USUARIO</a></li>
+    </ul>  
+</div>
